ec1f47b9f924e2caf0c23020a896ba09161efef2
ystessema <shawelllyordi@gmail.com>
2023-01-25 17:56:40 -0500

    This is my first commit.

---
+++ LSP_assi1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * Name: Yordanos Tessema
+ */
+
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+
+	}
+
+}
